23bf1c777f268f534b4ede63abfa8c059237bf3e
kadenyi <treykadenyi@gmail.com>
2022-11-16 17:06:47 +0300

    Add authorization

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,4 +1,6 @@
 class ExpendituresController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
     @expenditures = Expenditure.all
   end
---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :manage, :all, Category do |category|
+      category.user == user
+    end
+
+    can :manage, :all, Expenditure do |expense|
+      expense.user == user
+    end
+  end
+end
