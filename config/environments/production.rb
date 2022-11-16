3e46e02c04b1b2e39c269f887de3de7137209db9
kadenyi <treykadenyi@gmail.com>
2022-11-17 00:13:03 +0300

    Adjust devise forms

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # deployment link
+  config.action_mailer.default_url_options = { host: 'https://safe-refuge-72362.herokuapp.com/' }
 end
