8808b689a4695e6f0d32fed0b3923d93bcd6da29
kadenyi <treykadenyi@gmail.com>
2022-11-16 17:07:48 +0300

    Fix linter errors

--- app/controllers/category_expenditures_controller.rb
+++ app/controllers/category_expenditures_controller.rb
@@ -2,7 +2,7 @@ class CategoryExpendituresController < ApplicationController
   before_action :set_expenditure_category, only: %i[show edit update destroy]
 
   before_action :authenticate_user!
-  
+
   def index
     @category = Category.find(params[:category_id])
     @expenditures = @category.category_expenditures.includes(:expenditure).map(&:expenditure)
--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,6 +1,6 @@
 class ExpendituresController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @expenditures = Expenditure.all
   end
