41a153e31027ccd40bd97a454381e1a1e4366326
kadenyi <treykadenyi@gmail.com>
2022-11-16 19:41:46 +0300

    Remove images

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -2,7 +2,7 @@ class ExpendituresController < ApplicationController
   before_action :authenticate_user!
 
   def index; end
-  
+
   def show
     @expenditure = Expenditure.find(params[:id])
   end
